137300f09ced2ef93c0f93b800edf7173aaa83ec
Hsuan-Ling Huang <ssling0817@gmail.com>
2024-09-15 14:46:50 -0400

    add followup notifications, logging, button text(claimed by...), question list, get slack account info, get title

--- app.py
+++ app.py
@@ -6,9 +6,15 @@ from slack_sdk.errors import SlackApiError
 import requests
 from bot_config import *
 from piazza_api import Piazza
-
+import logging
+import os
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, message="The top-level `text` argument is missing")
 
 app = Flask(__name__)
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 client = WebClient(token=SLACK_TOKEN)
 piazza_id = "m0obli4e276tj" #TODO this is the suffix of the piazza url
@@ -18,66 +24,261 @@ p.user_login(email=piazza_email, password=piazza_password)
 network = p.network(piazza_id)
 
 
+
+#URL for posts on the page
+POST_BASE_URL = "https://piazza.com/class/"+piazza_id+"?cid="
+
+logging.basicConfig(filename=os.path.join(os.getcwd(), "runtime.log"), level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+def log_post(log_type, post_id, post_content, time, response, slack_user_id, slack_user_name):
+    """
+    log_type: int, 0 for getting AIDEN response log, 1 for submitting modified response log
+    """
+    # logger = logging.getLogger(__name__)
+    # logging.info(f"{log_type}, {post_id}, {post_content}, {time}, {response}, {slack_user_id}, {slack_user_name}")
+    try:
+        logging.info(f"{log_type}, {post_id}, {post_content}, {time}, {response}, {slack_user_id}, {slack_user_name}")
+    except Exception as e:
+        logging.error(f"Error logging post: {e}")
+
+
 @app.route('/slack/events', methods=['POST'])
 def handle_interaction():
-    payload = json.loads(request.form.get('payload'))
-    payload_type = payload['type']
-    if payload_type == 'block_actions':
-        button_name = payload['actions'][0]['text']['text']
-        if button_name == "Claim!":
-            click_time = datetime.datetime.now()
-            post_id = payload['actions'][0]['value'] 
-            
-            post = network.get_post(post_id)
-            # example post: {'history_size': 1, 'folders': ['hw1'], 'nr': 16, 'data': {'embed_links': []}, 'created': '2024-09-09T20:28:16Z', 'bucket_order': 3, 'no_answer_followup': 0, 'change_log': [{'anon': 'no', 'uid': 'lezv0orf1gz65e', 'data': 'm0vggb2k4ln6oj', 'v': 'all', 'type': 'create', 'when': '2024-09-09T20:28:16Z'}], 'bucket_name': 'Today', 'history': [{'anon': 'no', 'uid': 'lezv0orf1gz65e', 'subject': 'test8', 'created': '2024-09-09T20:28:16Z', 'content': 'test content of post'}], 'type': 'question', 'anon_map': {}, 'tags': ['hw1', 'instructor-question', 'unanswered'], '': [], 'unique_views': 2, 'children': [], 'tag_good_arr': [], 'no_answer': 1, 'anon_icons': True, 'id': 'm0vggb2evmf6oi', 'config': {'has_emails_sent': 1}, 'status': 'active', 'drafts': {}, 'request_instructor': 0, 'request_instructor_me': False, 'bookmarked': 1, 'num_favorites': 0, 'my_favorite': False, 'is_bookmarked': False, 'is_tag_good': False, 'q_edits': [], 'i_edits': [], 's_edits': [], 't': 1725913788302, 'default_anonymity': 'no'}
-            content = post['history'][0]['content']
-            llm_response = "This is a placeholder response from LLM API"
-            client.chat_postMessage(channel=payload['channel']['id'], text=f"Post_id: {post_id}\nContent: {content}\nClaimed the question at {click_time}.\nLLM Response: {llm_response}")
-            blocks = [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Question: " + content
-                    }
-                },
-                {
-                    "type": "input",
-                    "block_id": "content_input",
-                    "element": {
-                        "type": "plain_text_input",
-                        "action_id": "input_field",
-                        "initial_value": llm_response
-                    },
-                    "label": {
-                        "type": "plain_text",
-                        "text": "AIDEN response: Verify and modify the content if needed"
-                    }
-                },
-                {
-                    "type": "actions",
-                    "elements": [
+    if request.content_type == 'application/json':
+        data = request.json
+        # Respond to Slack's challenge verification
+        if 'challenge' in data:
+            return jsonify({'challenge': data['challenge']}), 200
+    
+        # Detect "list" message
+        if 'event' in data:
+            event = data['event']
+            # example data {'token': 'm0ACjdyEXmWnOdv9NLXvw0zP', 'team_id': 'T07KWGN6W5T', 'context_team_id': 'T07KWGN6W5T', 'context_enterprise_id': None, 'api_app_id': 'A07KZA9D8SX', 'event': {'user': 'U07KZD7M6LS', 'type': 'message', 'ts': '1726253641.515229', 'client_msg_id': '9497fb8f-f3cd-4ad6-ba36-d488c81cc605', 'text': 'list', 'team': 'T07KWGN6W5T', 'blocks': [{'type': 'rich_text', 'block_id': '3nsCA', 'elements': [{'type': 'rich_text_section', 'elements': [{'type': 'text', 'text': 'list'}]}]}], 'channel': 'C07KSQ27X70', 'event_ts': '1726253641.515229', 'channel_type': 'channel'}, 'type': 'event_callback', 'event_id': 'Ev07LYU0B6CX', 'event_time': 1726253641, 'authorizations': [{'enterprise_id': None, 'team_id': 'T07KWGN6W5T', 'user_id': 'U07KZADLXFV', 'is_bot': True, 'is_enterprise_install': False}], 'is_ext_shared_channel': False, 'event_context': '4-eyJldCI6Im1lc3NhZ2UiLCJ0aWQiOiJUMDdLV0dONlc1VCIsImFpZCI6IkEwN0taQTlEOFNYIiwiY2lkIjoiQzA3S1NRMjdYNzAifQ'}
+            if event.get('type') == 'message' and 'list' in event.get('text', '').lower():
+                channel_id = event.get('channel')
+                
+                # Query Piazza for unresolved posts (posts with no instructor response)              
+                unresolved_posts = [post for post in network.get_feed(limit=100000)['feed'] if 'no_answer' in post and post['no_answer'] == 1]
+                
+                # Format and send the response message
+                blocks = []
+                for post in unresolved_posts:
+                    post_id = post['nr']
+                    content_snippet = post['content_snipet']
+                    #example post: {'fol': 'hw1|', 'm': 1725917597682, 'rq': 0, 'id': 'm0virxf6cox1am', 'log': [{'t': '2024-09-09T21:33:17Z', 'u': 'lezv0orf1gz65e', 'n': 'create'}], 'unique_views': 3, 'score': 3.0, 'nid': 'm0obli4e276tj', 'is_new': False, 'version': 1, 'bucket_name': 'Last week', 'bucket_order': 6, 'folders': ['hw1'], 'nr': 27, 'main_version': 1, 'request_instructor': 0, 'subject': 'test19', 'no_answer_followup': 0, 'num_favorites': 0, 'type': 'question', 'tags': ['hw1', 'instructor-question', 'unanswered'], 'gd_f': 0, 'content_snipet': 'Test post id', 'view_adjust': 0, 'no_answer': 1, 'modified': '2024-09-09T21:33:17Z', 'updated': '2024-09-09T21:33:17Z', 'status': 'active'}
+                    title = post['subject']
+                    
+                    blocks.extend([
                         {
-                            "type": "button",
+                            "type": "section",
                             "text": {
                                 "type": "plain_text",
-                                "text": "Submit to Piazza"
-                            },
-                            "style": "primary",
-                            "value": str(post_id)
+                                "text": f"Post {post_id}\nTitle: "+title+"\n"+content_snippet,
+                            }
+                        },
+                        {
+                            "type": "section",
+                            "text": {
+                                "type": "mrkdwn",
+                                "text": f"<{POST_BASE_URL}{post_id}|Click here to view post {post_id}.>"
+                            }
+                        },
+                        {
+                            "type": "actions",
+                            "elements": [
+                                {
+                                    "type": "button",
+                                    "text": {
+                                        "type": "plain_text",
+                                        "text": "Generate AIDEN Response!"
+                                    },
+                                    "style": "primary",
+                                    "value": str(post_id),
+                                }
+                            ]
                         }
-                    ]
-                }
-            ]
-            result = client.chat_postMessage(
-                        channel=payload['channel']['id'],
-                        blocks=blocks  
+                    ])
+                # Send a Slack message containing the unresolved posts
+                try:
+                    client.chat_postMessage(
+                        channel=channel_id,
+                        blocks=blocks,
+                        text=''
                     )
-        else:
-            #get the edited content
-            post_id = payload['actions'][0]['value']
-            edited_content = payload['state']['values']['content_input']['input_field']['value']
-            network.create_instructor_answer(network.get_post(post_id), edited_content, 0)
+                except SlackApiError as e:
+                    print(f"Error posting message: {e}")
+
+    else:
+        payload = json.loads(request.form.get('payload'))
+        payload_type = payload['type']
+        # Detect Button Click
+        if payload_type == 'block_actions':
+            button_name = payload['actions'][0]['text']['text']
+            if button_name == "Generate AIDEN Response!":
+                claim_time = datetime.datetime.now()
+
+                #Extract Slack user info
+                user_id = payload['user']['id']
+                user_name = payload['user']['username']
+
+                #Extract post info
+                post_id = payload['actions'][0]['value'] 
+                post = network.get_post(post_id)
+                # example post: {'history_size': 1, 'folders': ['hw1'], 'nr': 16, 'data': {'embed_links': []}, 'created': '2024-09-09T20:28:16Z', 'bucket_order': 3, 'no_answer_followup': 0, 'change_log': [{'anon': 'no', 'uid': 'lezv0orf1gz65e', 'data': 'm0vggb2k4ln6oj', 'v': 'all', 'type': 'create', 'when': '2024-09-09T20:28:16Z'}], 'bucket_name': 'Today', 'history': [{'anon': 'no', 'uid': 'lezv0orf1gz65e', 'subject': 'test8', 'created': '2024-09-09T20:28:16Z', 'content': 'test content of post'}], 'type': 'question', 'anon_map': {}, 'tags': ['hw1', 'instructor-question', 'unanswered'], '': [], 'unique_views': 2, 'children': [], 'tag_good_arr': [], 'no_answer': 1, 'anon_icons': True, 'id': 'm0vggb2evmf6oi', 'config': {'has_emails_sent': 1}, 'status': 'active', 'drafts': {}, 'request_instructor': 0, 'request_instructor_me': False, 'bookmarked': 1, 'num_favorites': 0, 'my_favorite': False, 'is_bookmarked': False, 'is_tag_good': False, 'q_edits': [], 'i_edits': [], 's_edits': [], 't': 1725913788302, 'default_anonymity': 'no'}
+                content = post['history'][0]['content']
+                title = post['history'][0]['subject']
+                tags = post['tags']
+
+                llm_response = "This is a placeholder response from LLM API"
+                # client.chat_postMessage(channel=payload['channel']['id'], text=f"Post_id: {post_id}\nContent: {content}\nClaimed the question at {click_time}.\nLLM Response: {llm_response}")
+                blocks = [
+                    {
+                        "type": "header",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "AIDEN Response for Post" + post_id +"!"
+                        }
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Title: "+title+"\n"+content
+                        }
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": f"<{POST_BASE_URL}{post_id}|Click here to view post {post_id}.>"
+                        }
+                    },
+                    {
+                        "type": "input",
+                        "block_id": "content_input",
+                        "element": {
+                            "type": "plain_text_input",
+                            "action_id": "input_field",
+                            "initial_value": llm_response
+                        },
+                        "label": {
+                            "type": "plain_text",
+                            "text": "AIDEN response: (Verify and modify the content if needed)"
+                        }
+                    },
+                    {
+                        "type": "actions",
+                        "elements": [
+                            {
+                                "type": "button",
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Submit to Piazza"
+                                },
+                                "style": "primary",
+                                "value": str(post_id)
+                            }
+                        ]
+                    }
+                ]
+                original_blocks = payload['message']['blocks']
+                for block in original_blocks:
+                    if block['type'] == 'actions':  # Find the actions block
+                        for element in block['elements']:
+                            if element['type'] == 'button' and element['value']==str(post_id):  # Check if it's a button
+                                # Update the button text to show it was claimed by the user
+                                element['text']['text'] = "Claimed by " + user_name + "!"
+                result = client.chat_update(
+                    channel=payload['channel']['id'],
+                    ts=payload['container']['message_ts'],  # Timestamp of the original message
+                    blocks=original_blocks  # Use the modified blocks
+                )
+                result = client.chat_postMessage(
+                    channel=payload['channel']['id'],
+                    text=" ",
+                    blocks=blocks  
+                )
+                log_post(0, post_id, content, claim_time, llm_response, user_id, user_name)
+            else:
+                #Send the edited content to Piazza
+                post_time = datetime.datetime.now()
+
+                #Extract Slack user info
+                user_id = payload['user']['id']
+                user_name = payload['user']['username']
+
+                #Extract post info
+                post_id = payload['actions'][0]['value']
+                post = network.get_post(post_id)
+                content = post['history'][0]['content']
+                title = post['history'][0]['subject']
+
+                edited_content = payload['state']['values']['content_input']['input_field']['value']
+                network.create_instructor_answer(network.get_post(post_id), edited_content, 0)
+                # client.chat_postMessage(channel=payload['channel']['id'], text=f"Submitted answer to Piazza for post {post_id}!")
+                try:
+                    client.chat_update(
+                        channel=payload['channel']['id'],
+                        ts=payload['container']['message_ts'],  # This is the timestamp of the original message to be updated
+                        text=" ",  # Update the message text
+                        blocks=[
+                            {
+                                "type": "header",
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "AIDEN Response for Post" + post_id +"!"
+                                }
+                            },
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Title: "+title+"\n"+content
+                                }
+                            },
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": f"<{POST_BASE_URL}{post_id}|Click here to view post {post_id}.>"
+                                }
+                            },
+                            {
+                                "type": "input",
+                                "block_id": "content_input",
+                                "element": {
+                                    "type": "plain_text_input",
+                                    "action_id": "input_field",
+                                    "initial_value": edited_content
+                                },
+                                "label": {
+                                    "type": "plain_text",
+                                    "text": "AIDEN response: (Verify and modify the content if needed)"
+                                }
+                            },
+                            {
+                                "type": "actions",
+                                "elements": [
+                                    {
+                                        "type": "button",
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "Submitted by " + user_name + "!"
+                                        },
+                                        "style": "primary",
+                                        "value": str(post_id)
+                                    }
+                                ]
+                            }
+                        ]
+                    )
+                except SlackApiError as e:
+                    logging.error(f"Error updating message: {e}")
+                log_post(1, post_id, content, post_time, edited_content, user_id, user_name)
+        
+        
+            
+            
+    
     return '', 200
 
 if __name__ == '__main__':
--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ from piazza_api import Piazza
 from time import sleep
 import logging
 import os
+import sys
 # Import WebClient from Python SDK (github.com/slackapi/python-slack-sdk)
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
@@ -18,7 +19,7 @@ from slack_sdk.models.blocks import SectionBlock, ActionsBlock, ButtonElement
 # WebClient instantiates a client that can call API methods
 # When using Bolt, you can use either `app.client` or the `client` passed to listeners.
 client = WebClient(token=SLACK_TOKEN)
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
 #Accessing Piazza and loading data
 piazza_id = "m0obli4e276tj" #TODO this is the suffix of the piazza url
@@ -29,69 +30,139 @@ network = p.network(piazza_id)
 
 #Accessing Slack and configuring the bot
 channel="aiden-bot" #TODO Name of the channel to post to
-
-
+last_seen_followups = {}
+unresolved_posts = set()
 #URL for posts on the page
 POST_BASE_URL = "https://piazza.com/class/"+piazza_id+"?cid="
-
 def get_max_id(feed):
     for post in feed:
         if "pin" not in post:
             return post["nr"]
     return -1
-
+# def check_for_followup(network):
 def check_for_new_posts(LAST_ID,network=network,include_link=True):
     while True:
         try:
-            UPDATED_LAST_ID = get_max_id(network.get_feed()['feed'])
+            feed = network.get_feed(limit=100000)['feed'] # get a list of posts for this nedwork
+            # Send New Post Notification
+            UPDATED_LAST_ID = get_max_id(feed)
+            
             if UPDATED_LAST_ID > LAST_ID:
-                attachment = None
-                message = None
-                if include_link is True:
+                new_posts = [post for post in feed if post["nr"] > LAST_ID]
+                for post in new_posts:
+                    post_id = post["nr"]
+                    unresolved_posts.add(post_id)
+                    post_content = post['content_snipet']#network.get_post(post_id)['history'][0]['content']
+                    attachment = None
+                    message = None
+                    if include_link is True:
+                        blocks =[
+                            {
+                                "type": "header",
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "New post!"
+                                }
+                            },
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Post:\n"+post_content
+                                }
+                            },
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": f"<{POST_BASE_URL}{post_id}|Click here to view post {post_id}.>"
+                                }
+                            },
+                            {
+                                "type": "actions",
+                                "elements": [
+                                    {
+                                        "type": "button",
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "Generate AIDEN Response!"
+                                        },
+                                        "style": "primary",
+                                        "value": str(post_id),
+                                    }
+                                ]
+                            }
+                        ]
+                        
+                    else:
+                        message="New post on Piazza!"
+                    try:
+                        result = client.chat_postMessage(
+                            channel=channel,
+                            text=message,
+                            blocks=blocks  
+                        )
+                        # logger.info(result)
+
+                    except SlackApiError as e:
+                        print(f"Error posting message: {e}")
+                LAST_ID = UPDATED_LAST_ID
+
+            #Send Follow-up Notification
+            for post in feed:
+                post_id = post["nr"]
+                #example post: {'fol': 'hw1|', 'm': 1726168643754, 'rq': 0, 'id': 'm0zo6tyerln7nn', 'my_post': True, 'log': [{'t': '2024-09-12T19:15:55Z', 'u': 'm0obm891qax1ib', 'n': 'create'}, {'t': '2024-09-12T19:17:23Z', 'u': 'm0obm891qax1ib', 'n': 'followup'}], 'unique_views': 1, 'score': 1.0, 'nid': 'm0obli4e276tj', 'is_new': False, 'version': 2, 'book': 1, 'bucket_name': 'Today', 'bucket_order': 3, 'folders': ['hw1'], 'nr': 41, 'main_version': 2, 'request_instructor': 0, 'subject': 'log test', 'no_answer_followup': 1, 'num_favorites': 0, 'type': 'question', 'tags': ['hw1', 'instructor-question', 'unanswered'], 'gd_f': 0, 'content_snipet': 'test', 'view_adjust': 0, 'no_answer': 1, 'modified': '2024-09-12T19:17:23Z', 'updated': '2024-09-12T19:15:55Z', 'status': 'active'}
+                num_followups = sum(1 for entry in post['log'] if entry['n'] == 'followup')
+                # print("Post ID: ", post_id, "Number of followups: ", num_followups)
+                # print(post)
+                # print()
+                # num_followups = len(network.get_post(post_id)['children'])  
+                if post_id not in last_seen_followups:
+                    last_seen_followups[post_id] = num_followups
+                if num_followups > last_seen_followups[post_id]:
+                    last_seen_followups[post_id] = num_followups
+                    post_content = post['content_snipet']#network.get_post(post_id)['history'][0]['content']
                     blocks =[
                         {
                             "type": "header",
                             "text": {
                                 "type": "plain_text",
-                                "text": "New post!"
+                                "text": "New follow-up on post"+str(post_id)+"!"
                             }
                         },
                         {
-                            "type": "actions",
-                            "elements": [
-                                {
-                                    "type": "button",
-                                    "text": {
-                                        "type": "plain_text",
-                                        "text": "Claim!"
-                                    },
-                                    "style": "primary",
-                                    "value": str(UPDATED_LAST_ID),
-                                }
-                            ]
+                            "type": "section",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Post:\n"+post_content
+                            }
+                        },
+                        {
+                            "type": "section",
+                            "text": {
+                                "type": "mrkdwn",
+                                "text": f"<{POST_BASE_URL}{post_id}|Click here to view post {str(post_id)}.>"
+                            }
                         }
                     ]
-                    
-                else:
-                    message="New post on Piazza!"
-                try:
-                    result = client.chat_postMessage(
-                        channel=channel,
-                        text=message,
-                        blocks=blocks  
-                    )
-                    logger.info(result)
+                    try:
+                        result = client.chat_postMessage(
+                            channel=channel,
+                            text=None,
+                            blocks=blocks  
+                        )
+                        # logger.info(result)
 
-                except SlackApiError as e:
-                    logger.error(f"Error posting message: {e}")
-                LAST_ID = UPDATED_LAST_ID
+                    except SlackApiError as e:
+                        print(f"Error posting message: {e}")
+                # sleep(0.5)
             else:
                 pass
             print("Slackbot is running...")
-            sleep(30)
-        except:
-            print("Error when attempting to get Piazza feed, going to sleep...")
-            sleep(30)
+            sleep(10)
+        except Exception as e:
+            print("Error when attempting to get Piazza feed, going to sleep.... Error message: ", e)
+            sleep(10)
 
 if __name__ == '__main__':
     LAST_ID = get_max_id(network.get_feed()['feed'])
